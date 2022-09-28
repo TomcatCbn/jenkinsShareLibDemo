864cb497636f32cbb80bb756df113f9ba32900e6
UVAW4PV <Boning.Chen@mobilityasia.com.cn>
2022-09-28 21:37:12 +0800

    test

--- src/com/dssomobile/jenkins/stages/BuildStage.groovy
+++ src/com/dssomobile/jenkins/stages/BuildStage.groovy
@@ -3,22 +3,23 @@ package com.dssomobile.jenkins.stages
 import com.dssomobile.jenkins.models.CodeProjectDo
 import com.dssomobile.jenkins.models.SettingsDo
 
-def name() {
-    return 'Build Stage'
-}
+def name = 'Build Stage'
 
 def buildAndroid(CodeProjectDo mainProject, SettingsDo settings) {
-    log.i("${name()}: 执行buildAndroid")
+    log.i("${name}: 执行buildAndroid")
     script {
-        dir(settings.defaultPackageDir)
-        log.i "begin build ${mainProject.key}"
+        dir(settings.defaultPackageDir) {
+            log.i "begin build ${mainProject.key}"
+            sh "flutter build apk"
+            archiveArtifacts artifacts: 'build/app/outputs/apk/**/*.apk'
+        }
+
 //        sh "dart pub global activate melos"
 //        sh "melos clean"
 //        sh "melos bootstrap"
 //        dir('packages/dssomobile-oneapp-app-main') {
 //            echo 'Begin build app'
-//            sh "flutter build apk"
-//            archiveArtifacts artifacts: 'build/app/outputs/apk/**/*.apk'
+//
 //        }
     }
 }
