e65814cc3d5f740310bf77c1c2a809b266bb940d
UVAW4PV <Boning.Chen@mobilityasia.com.cn>
2022-09-28 23:17:47 +0800

    test

--- vars/dartToken.groovy
+++ vars/dartToken.groovy
@@ -1,7 +1,7 @@
 // dart token pub add
 
 def call(String repositoryUrl, String tokenEnvVar){
-    sh "export TOKEN_FOR_DART=${env.TOKEN_JFROG}"
+    sh "export TOKEN_FOR_DART=${env.TOKEN_JFROG} && dart pub token add $repositoryUrl --env-var TOKEN_FOR_DART"
 //    sh "dart pub token add $repositoryUrl --env-var $tokenEnvVar"
-    sh "dart pub token add $repositoryUrl --env-var TOKEN_FOR_DART"
+//    sh "dart pub token add $repositoryUrl --env-var TOKEN_FOR_DART"
 }
