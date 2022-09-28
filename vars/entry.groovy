bdb0e9d42e03013bcecd66261c3afe8bb6127661
UVAW4PV <Boning.Chen@mobilityasia.com.cn>
2022-09-28 22:22:35 +0800

    test

--- vars/entry.groovy
+++ vars/entry.groovy
@@ -35,7 +35,7 @@ def call() {
                 steps {
                     script {
                         log.i("Stage: ${buildStage.name()} begin...")
-                        log.d("build package: ${Config.codeProjects[mainProject]}")
+                        log.i("build package: ${Config.codeProjects[mainProject]}")
                         buildStage.buildAndroid(Config.codeProjects[mainProject], Config.settings)
                     }
                 }
