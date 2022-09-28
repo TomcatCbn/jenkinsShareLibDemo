370ff553ffea702c506144c57a6b9583d8d511ae
UVAW4PV <Boning.Chen@mobilityasia.com.cn>
2022-09-28 22:31:06 +0800

    test

--- src/com/dssomobile/jenkins/models/SettingsDo.groovy
+++ src/com/dssomobile/jenkins/models/SettingsDo.groovy
@@ -3,8 +3,4 @@ package com.dssomobile.jenkins.models
 class SettingsDo {
     // 默认仓库代码存放的位置
     String defaultPackageDir
-
-    def toString() {
-        return "defaultPackageDir=$defaultPackageDir"
-    }
 }
