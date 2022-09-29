1afb725088e73de57febbc44712a62140a893213
UVAW4PV <Boning.Chen@mobilityasia.com.cn>
2022-09-29 19:38:07 +0800

    test

--- src/com/dssomobile/jenkins/models/DynamicDataDo.groovy
+++ src/com/dssomobile/jenkins/models/DynamicDataDo.groovy
@@ -1,4 +1,6 @@
-import  com.dssomobile.jenkins.models.CodeProjectDo
+package com.dssomobile.jenkins.models
+
+import com.dssomobile.jenkins.models.CodeProjectDo
 
 class DynamicDataDo {
     // 当前要执行操作的code project
