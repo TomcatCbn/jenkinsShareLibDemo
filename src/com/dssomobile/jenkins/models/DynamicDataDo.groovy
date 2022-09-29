1396b21946ba7736c02e9a30525098a7617db461
UVAW4PV <Boning.Chen@mobilityasia.com.cn>
2022-09-29 23:20:10 +0800

    test

--- src/com/dssomobile/jenkins/models/DynamicDataDo.groovy
+++ src/com/dssomobile/jenkins/models/DynamicDataDo.groovy
@@ -6,6 +6,6 @@ import com.dssomobile.jenkins.models.CodeProjectDo
 class DynamicDataDo {
 
     // 当前要执行操作的code project
-    List<CodeProjectDo> selectedProject
+    List<CodeProjectDo> selectedProject = []
 
 }
