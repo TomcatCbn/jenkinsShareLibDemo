fe5d18c01c6696d2ee620e6df93e66726018ec9e
UVAW4PV <Boning.Chen@mobilityasia.com.cn>
2022-09-29 13:05:09 +0800

    test

--- vars/oneAppConfig.groovy
+++ vars/oneAppConfig.groovy
@@ -28,7 +28,7 @@ def app_main() {
         cp.key = 'dssomobile-oneapp-app-main'
         cp.repoUrl = 'https://gitlab-rd0.maezia.com/dssomobile/oneapp/dssomobile-oneapp-app-main.git'
 
-        def pack1 = DartPackageDo()
+        def pack1 = new DartPackageDo()
         pack1.relativePath = '.'
         pack1.packageName = 'app_main'
         cp.packages = [pack1]
@@ -41,7 +41,7 @@ def app_account() {
         cp.key = 'dssomobile-oneapp-app-account'
         cp.repoUrl = 'https://gitlab-rd0.maezia.com/dssomobile/oneapp/dssomobile-oneapp-app-account.git'
 
-        def pack1 = DartPackageDo()
+        def pack1 = new DartPackageDo()
         pack1.relativePath = '.'
         pack1.packageName = 'app_account'
         cp.packages = [pack1]
@@ -54,7 +54,7 @@ def app_maintenance() {
         cp.key = 'dssomobile-oneapp-app-maintenance'
         cp.repoUrl = 'https://gitlab-rd0.maezia.com/dssomobile/oneapp/dssomobile-oneapp-app-maintenance.git'
 
-        def pack1 = DartPackageDo()
+        def pack1 = new DartPackageDo()
         pack1.relativePath = 'app_maintenance'
         pack1.packageName = 'app_maintenance'
         cp.packages = [pack1]
@@ -67,7 +67,7 @@ def app_media() {
         cp.key = 'dssomobile-oneapp-app-media'
         cp.repoUrl = 'https://gitlab-rd0.maezia.com/dssomobile/oneapp/dssomobile-oneapp-app-media.git'
 
-        def pack1 = DartPackageDo()
+        def pack1 = new DartPackageDo()
         pack1.relativePath = '.'
         pack1.packageName = 'app_media'
         cp.packages = [pack1]
@@ -80,7 +80,7 @@ def app_order() {
         cp.key = 'dssomobile-oneapp-app-order'
         cp.repoUrl = 'https://gitlab-rd0.maezia.com/dssomobile/oneapp/dssomobile-oneapp-app-order.git'
 
-        def pack1 = DartPackageDo()
+        def pack1 = new DartPackageDo()
         pack1.relativePath = '.'
         pack1.packageName = 'app_order'
         cp.packages = [pack1]
@@ -93,7 +93,7 @@ def app_setting() {
         cp.key = 'dssomobile-oneapp-app-setting'
         cp.repoUrl = 'https://gitlab-rd0.maezia.com/dssomobile/oneapp/dssomobile-oneapp-app-setting.git'
 
-        def pack1 = DartPackageDo()
+        def pack1 = new DartPackageDo()
         pack1.relativePath = '.'
         pack1.packageName = 'app_setting'
         cp.packages = [pack1]
@@ -106,7 +106,7 @@ def car_app() {
         cp.key = 'dssomobile-oneapp-car-app'
         cp.repoUrl = 'https://gitlab-rd0.maezia.com/dssomobile/oneapp/dssomobile-oneapp-car-app.git'
 
-        def pack1 = DartPackageDo()
+        def pack1 = new DartPackageDo()
         pack1.relativePath = '.'
         pack1.packageName = 'app_car'
         cp.packages = [pack1]
@@ -119,7 +119,7 @@ def car_vehicle() {
         cp.key = 'dssomobile-oneapp-car-vehicle'
         cp.repoUrl = 'https://gitlab-rd0.maezia.com/dssomobile/oneapp/dssomobile-oneapp-car-vehicle.git'
 
-        def pack1 = DartPackageDo()
+        def pack1 = new DartPackageDo()
         pack1.relativePath = '.'
         pack1.packageName = 'vehicle'
         cp.packages = [pack1]
@@ -132,7 +132,7 @@ def car_services() {
         cp.key = 'dssomobile-oneapp-car-services'
         cp.repoUrl = 'https://gitlab-rd0.maezia.com/dssomobile/oneapp/dssomobile-oneapp-car-services.git'
 
-        def pack1 = DartPackageDo()
+        def pack1 = new DartPackageDo()
         pack1.relativePath = '.'
         pack1.packageName = 'services'
         cp.packages = [pack1]
@@ -145,7 +145,7 @@ def car_connector() {
         cp.key = 'dssomobile-oneapp-car-connector'
         cp.repoUrl = 'https://gitlab-rd0.maezia.com/dssomobile/oneapp/dssomobile-oneapp-car-connector.git'
 
-        def pack1 = DartPackageDo()
+        def pack1 = new DartPackageDo()
         pack1.relativePath = '.'
         pack1.packageName = 'connector'
         cp.packages = [pack1]
@@ -158,11 +158,11 @@ def ui_basic() {
         cp.key = 'dssomobile-oneapp-ui-basic'
         cp.repoUrl = 'https://gitlab-rd0.maezia.com/dssomobile/oneapp/dssomobile-oneapp-ui-basic.git'
 
-        def pack1 = DartPackageDo()
+        def pack1 = new DartPackageDo()
         pack1.relativePath = '.'
         pack1.packageName = 'ui_basic'
 
-        def pack2 = DartPackageDo()
+        def pack2 = new DartPackageDo()
         pack2.relativePath = 'packages/ui_basic_annotation'
         pack2.packageName = 'ui_basic_annotation'
         cp.packages = [pack1, pack2]
@@ -175,7 +175,7 @@ def ui_business() {
         cp.key = 'dssomobile-oneapp-ui-business'
         cp.repoUrl = 'https://gitlab-rd0.maezia.com/dssomobile/oneapp/dssomobile-oneapp-ui-business.git'
 
-        def pack1 = DartPackageDo()
+        def pack1 = new DartPackageDo()
         pack1.relativePath = '.'
         pack1.packageName = 'ui_business'
         cp.packages = [pack1]
@@ -188,7 +188,7 @@ def clr_setting() {
         cp.key = 'dssomobile-oneapp-clr-setting'
         cp.repoUrl = 'https://gitlab-rd0.maezia.com/dssomobile/oneapp/dssomobile-oneapp-clr-setting.git'
 
-        def pack1 = DartPackageDo()
+        def pack1 = new DartPackageDo()
         pack1.relativePath = '.'
         pack1.packageName = 'clr_setting'
         cp.packages = [pack1]
@@ -201,7 +201,7 @@ def clr_payment() {
         cp.key = 'dssomobile-oneapp-clr-payment'
         cp.repoUrl = 'https://gitlab-rd0.maezia.com/dssomobile/oneapp/dssomobile-oneapp-clr-payment.git'
 
-        def pack1 = DartPackageDo()
+        def pack1 = new DartPackageDo()
         pack1.relativePath = '.'
         pack1.packageName = 'clr_payment'
         cp.packages = [pack1]
@@ -214,7 +214,7 @@ def clr_order() {
         cp.key = 'dssomobile-oneapp-clr-order'
         cp.repoUrl = 'https://gitlab-rd0.maezia.com/dssomobile/oneapp/dssomobile-oneapp-clr-order.git'
 
-        def pack1 = DartPackageDo()
+        def pack1 = new DartPackageDo()
         pack1.relativePath = '.'
         pack1.packageName = 'clr_order'
         cp.packages = [pack1]
@@ -227,7 +227,7 @@ def clr_media() {
         cp.key = 'dssomobile-oneapp-clr-media'
         cp.repoUrl = 'https://gitlab-rd0.maezia.com/dssomobile/oneapp/dssomobile-oneapp-clr-media.git'
 
-        def pack1 = DartPackageDo()
+        def pack1 = new DartPackageDo()
         pack1.relativePath = '.'
         pack1.packageName = 'clr_media'
         cp.packages = [pack1]
@@ -240,7 +240,7 @@ def clr_maintenance() {
         cp.key = 'dssomobile-oneapp-clr-maintenance'
         cp.repoUrl = 'https://gitlab-rd0.maezia.com/dssomobile/oneapp/dssomobile-oneapp-clr-maintenance.git'
 
-        def pack1 = DartPackageDo()
+        def pack1 = new DartPackageDo()
         pack1.relativePath = 'clr_maintenance'
         pack1.packageName = 'clr_maintenance'
         cp.packages = [pack1]
@@ -253,7 +253,7 @@ def clr_geo() {
         cp.key = 'dssomobile-oneapp-clr-geo'
         cp.repoUrl = 'https://gitlab-rd0.maezia.com/dssomobile/oneapp/dssomobile-oneapp-clr-geo.git'
 
-        def pack1 = DartPackageDo()
+        def pack1 = new DartPackageDo()
         pack1.relativePath = 'clr_geo'
         pack1.packageName = 'clr_geo'
         cp.packages = [pack1]
@@ -266,7 +266,7 @@ def clr_account() {
         cp.key = 'dssomobile-oneapp-clr-account'
         cp.repoUrl = 'https://gitlab-rd0.maezia.com/dssomobile/oneapp/dssomobile-oneapp-clr-account.git'
 
-        def pack1 = DartPackageDo()
+        def pack1 = new DartPackageDo()
         pack1.relativePath = '.'
         pack1.packageName = 'clr_account'
         cp.packages = [pack1]
@@ -279,7 +279,7 @@ def basic_webview() {
         cp.key = 'dssomobile-oneapp-basic-webview'
         cp.repoUrl = 'https://gitlab-rd0.maezia.com/dssomobile/oneapp/dssomobile-oneapp-basic-webview.git'
 
-        def pack1 = DartPackageDo()
+        def pack1 = new DartPackageDo()
         pack1.relativePath = 'basic_webview'
         pack1.packageName = 'basic_webview'
         cp.packages = [pack1]
@@ -292,7 +292,7 @@ def basic_theme() {
         cp.key = 'dssomobile-oneapp-basic-theme'
         cp.repoUrl = 'https://gitlab-rd0.maezia.com/dssomobile/oneapp/dssomobile-oneapp-basic-theme.git'
 
-        def pack1 = DartPackageDo()
+        def pack1 = new DartPackageDo()
         pack1.relativePath = 'basic_theme'
         pack1.packageName = 'basic_theme'
         cp.packages = [pack1]
@@ -305,10 +305,10 @@ def basic_storage() {
         cp.key = 'dssomobile-oneapp-basic-storage'
         cp.repoUrl = 'https://gitlab-rd0.maezia.com/dssomobile/oneapp/dssomobile-oneapp-basic-storage.git'
 
-        def pack1 = DartPackageDo()
+        def pack1 = new DartPackageDo()
         pack1.relativePath = 'basic-storage'
         pack1.packageName = 'basic_storage'
-        def pack2 = DartPackageDo()
+        def pack2 = new DartPackageDo()
         pack2.relativePath = 'basic-storage-generator'
         pack2.packageName = 'basic_storage_generator'
         cp.packages = [pack1, pack2]
@@ -321,15 +321,15 @@ def basic_resource() {
         cp.key = 'dssomobile-oneapp-basic-resource'
         cp.repoUrl = 'https://gitlab-rd0.maezia.com/dssomobile/oneapp/dssomobile-oneapp-basic-resource.git'
 
-        def pack1 = DartPackageDo()
+        def pack1 = new DartPackageDo()
         pack1.relativePath = '.'
         pack1.packageName = 'basic_resource'
 
-        def pack2 = DartPackageDo()
+        def pack2 = new DartPackageDo()
         pack2.relativePath = 'packages/clr_obs_access'
         pack2.packageName = 'clr_obs_access'
 
-        def pack3 = DartPackageDo()
+        def pack3 = new DartPackageDo()
         pack3.relativePath = 'packages/ui_resource'
         pack3.packageName = 'ui_resource'
 
@@ -343,7 +343,7 @@ def basic_push() {
         cp.key = 'dssomobile-oneapp-basic-push'
         cp.repoUrl = 'https://gitlab-rd0.maezia.com/dssomobile/oneapp/dssomobile-oneapp-basic-push.git'
 
-        def pack1 = DartPackageDo()
+        def pack1 = new DartPackageDo()
         pack1.relativePath = '.'
         pack1.packageName = 'basic_push'
         cp.packages = [pack1]
@@ -356,11 +356,11 @@ def basic_platform() {
         cp.key = 'dssomobile-oneapp-basic-platform'
         cp.repoUrl = 'https://gitlab-rd0.maezia.com/dssomobile/oneapp/dssomobile-oneapp-basic-platform.git'
 
-        def pack1 = DartPackageDo()
+        def pack1 = new DartPackageDo()
         pack1.relativePath = '.'
         pack1.packageName = 'basic_platform'
 
-        def pack2 = DartPackageDo()
+        def pack2 = new DartPackageDo()
         pack2.relativePath = 'packages/permission_annotation_generator'
         pack2.packageName = 'permission_annotation_generator'
 
@@ -374,7 +374,7 @@ def basic_network() {
         cp.key = 'dssomobile-oneapp-basic-network'
         cp.repoUrl = 'https://gitlab-rd0.maezia.com/dssomobile/oneapp/dssomobile-oneapp-basic-network.git'
 
-        def pack1 = DartPackageDo()
+        def pack1 = new DartPackageDo()
         pack1.relativePath = '.'
         pack1.packageName = 'basic_network'
         cp.packages = [pack1]
@@ -387,15 +387,15 @@ def basic_modular() {
         cp.key = 'dssomobile-oneapp-basic-modular'
         cp.repoUrl = 'https://gitlab-rd0.maezia.com/dssomobile/oneapp/dssomobile-oneapp-basic-modular.git'
 
-        def pack1 = DartPackageDo()
+        def pack1 = new DartPackageDo()
         pack1.relativePath = '.'
         pack1.packageName = 'basic_modular'
 
-        def pack2 = DartPackageDo()
+        def pack2 = new DartPackageDo()
         pack2.relativePath = 'packages/basic_modular_generator'
         pack2.packageName = 'basic_modular_generator'
 
-        def pack3 = DartPackageDo()
+        def pack3 = new DartPackageDo()
         pack3.relativePath = 'packages/basic_modular_route'
         pack3.packageName = 'basic_modular_route'
 
@@ -409,7 +409,7 @@ def basic_logger() {
         cp.key = 'dssomobile-oneapp-basic-logger'
         cp.repoUrl = 'https://gitlab-rd0.maezia.com/dssomobile/oneapp/dssomobile-oneapp-basic-logger.git'
 
-        def pack1 = DartPackageDo()
+        def pack1 = new DartPackageDo()
         pack1.relativePath = '.'
         pack1.packageName = 'basic_logger'
         cp.packages = [pack1]
@@ -422,19 +422,19 @@ def basic_intl() {
         cp.key = 'dssomobile-oneapp-basic-intl'
         cp.repoUrl = 'https://gitlab-rd0.maezia.com/dssomobile/oneapp/dssomobile-oneapp-basic-intl.git'
 
-        def pack1 = DartPackageDo()
+        def pack1 = new DartPackageDo()
         pack1.relativePath = 'intl'
         pack1.packageName = 'basic_intl'
 
-        def pack2 = DartPackageDo()
+        def pack2 = new DartPackageDo()
         pack2.relativePath = 'intl_localizely'
         pack2.packageName = 'basic_intl_localizely'
 
-        def pack3 = DartPackageDo()
+        def pack3 = new DartPackageDo()
         pack3.relativePath = 'intl_flutter'
         pack3.packageName = 'basic_intl_flutter'
 
-        def pack4 = DartPackageDo()
+        def pack4 = new DartPackageDo()
         pack4.relativePath = 'intl_utils'
         pack4.packageName = 'basic_intl_utils'
 
@@ -448,7 +448,7 @@ def basic_error() {
         cp.key = 'dssomobile-oneapp-basic-error'
         cp.repoUrl = 'https://gitlab-rd0.maezia.com/dssomobile/oneapp/dssomobile-oneapp-basic-error.git'
 
-        def pack1 = DartPackageDo()
+        def pack1 = new DartPackageDo()
         pack1.relativePath = '.'
         pack1.packageName = 'basic_error'
         cp.packages = [pack1]
