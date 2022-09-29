9bbdcf9677d7f11cfdbdc662733b991ae102aea5
UVAW4PV <Boning.Chen@mobilityasia.com.cn>
2022-09-29 22:52:44 +0800

    test

--- vars/dartToken.groovy
+++ vars/dartToken.groovy
@@ -3,6 +3,6 @@
 // 给[repositoryUrl]用环境变量[tokenEnvVar]进行授权
 def call(String repositoryUrl, String tokenEnvVar){
     // 先移除，再添加
-    sh "dart pub token remove $repositoryUrl"
+    sh "dart pub token remove $repositoryUrl || true"
     sh "dart pub token add $repositoryUrl --env-var $tokenEnvVar"
 }
