a2cb5179cd2884319591d87ba8df0b3e62690151
UVAW4PV <Boning.Chen@mobilityasia.com.cn>
2022-09-29 23:11:15 +0800

    test

--- vars/readProperty.groovy
+++
@@ -1,8 +0,0 @@
-def readProperties(propertiesFile) {
-	 def props = readProperties interpolate: true, file: propertiesFile
-	 props.each {
-		println ( it.key + " = " + it.value )
-	 }
-
-     return props
-}
